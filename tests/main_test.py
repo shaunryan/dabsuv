73b1d0f4bd67b4a9eea913e8863beb7d61600d51
Shaun Ryan <shaun_chiburi@hotmail.com>
2025-06-28 06:27:48 +0100

    Update main_test.py

--- tests/main_test.py
+++ tests/main_test.py
@@ -1,5 +1,6 @@
-from manisha.taxi import get_taxis
+from manisha.taxi import _get_taxis, _get_spark
 
+# duumy test
 def test_main():
     taxis = get_taxis(_get_spark())
-    assert taxis.count() == 5
+    assert taxis.count() > 5
